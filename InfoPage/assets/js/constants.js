6c5356c79e0cc493268a78fb81d6d7512fbbf34c
kusogray <kusogray1@gmail.com>
2015-08-30 10:53:04 +0800

    multi language intro page template
    wait to fill the contents

---
+++ InfoPage/assets/js/constants.js
@@ -0,0 +1,36 @@
+﻿// Language Constants
+// 繁體中文
+var znTwHome = "主頁";
+
+//簡體中文
+var znChHome = "主页";
+
+// 日本語
+var jpHome = "ホーム";
+
+// English
+var enHome = "Main Page";
+
+var language = "jp";
+var languagePackage = {};
+
+if (language == "en") {
+	languagePackage = {
+		homeText : enHome
+	};
+} else if (language == "jp") {
+	languagePackage = {
+		homeText : jpHome
+	};
+}
+
+var textSetter = function (id, text) {
+	var id = "#" + id;
+	$(id).html($(id).html().replace("ReplaceText", text));
+}
+
+$(document).ready(function () {
+	// Handler for .ready() called.
+	textSetter("homeText", languagePackage.homeText);
+
+});
